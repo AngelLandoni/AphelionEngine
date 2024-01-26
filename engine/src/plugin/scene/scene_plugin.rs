495d72ae1214806594c88d6c608e2c64b47ae971
Angel Landoni <angel.landoni.usa@gmail.com>
2024-01-26 20:00:39 +0100

    cursor: Reset delta when frame ends

--- engine/src/plugin/scene/scene_plugin.rs
+++ engine/src/plugin/scene/scene_plugin.rs
@@ -13,10 +13,7 @@ pub struct ScenePlugin;
 
 impl Pluggable for ScenePlugin {
     fn configure(&self, app: &mut App) {
-        // Setup camera component.
         app.world.add_unique(Camera::default());
-        // Setup the perspective componente, this component will be used in
-        // conjuntion with the camera.
         app.world.add_unique(Perspective::default());
         app.world.add_unique(Keyboard::default());
         app.world.add_unique(Cursor::default());
