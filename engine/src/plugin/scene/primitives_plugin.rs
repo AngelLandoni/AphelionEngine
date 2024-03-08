a7ba7204e5f00fb463e62a6f32008dbe90de8950
Angel Landoni <angel.landoni.usa@gmail.com>
2024-03-08 12:58:38 +0100

    engine:editor: Add support for cone mesh

--- engine/src/plugin/scene/primitives_plugin.rs
+++ engine/src/plugin/scene/primitives_plugin.rs
@@ -44,10 +44,12 @@ impl Pluggable for PrimitivesPlugin {
         configure_pentagon_primitive(&gpu, &mut a_server);
         configure_cube_primitive(&gpu, &mut a_server);
         configure_sphere_primitive(&gpu, &mut a_server);
+        configure_plane_primitive(&gpu, &mut a_server);
+        configure_cone_primitive(&gpu, &mut a_server);
     }
 }
 
-/// Conatins the id used to retrieve the information from the `AssetServer`.
+// PENTAGON
 pub const PENTAGON_PRIMITIVE_ID: &str = "PENTAGON_PRIMITIVE_MESH";
 
 const PENTAGON_VERTICES: &[Vertex] = &[
@@ -91,7 +93,16 @@ fn configure_pentagon_primitive(gpu: &AbstractGpu, a_server: &mut AssetServer) {
     a_server.register_mesh(PENTAGON_PRIMITIVE_ID.to_owned(), mesh);
 }
 
+// CUBE
 pub const CUBE_PRIMITIVE_ID: &str = "CUBE_PRIMITIVE_MESH";
+
+pub fn cube_mesh_resource() -> MeshResourceID {
+    MeshResourceID(CUBE_PRIMITIVE_ID.to_owned())
+}
+
+pub fn cube_mesh_component() -> MeshComponent {
+    MeshComponent(cube_mesh_resource())
+}
 
 const CUBE_VERTICES: &[Vertex] = &[
     Vertex {
@@ -222,15 +233,9 @@ fn configure_cube_primitive(gpu: &AbstractGpu, a_server: &mut AssetServer) {
     a_server.register_mesh(CUBE_PRIMITIVE_ID.to_owned(), mesh);
 }
 
-pub fn cube_mesh_resource() -> MeshResourceID {
-    MeshResourceID(CUBE_PRIMITIVE_ID.to_owned())
-}
-
-pub fn cube_mesh_component() -> MeshComponent {
-    MeshComponent(cube_mesh_resource())
-}
-
+// SPHERE
 pub const SPHERE_PRIMITIVE_ID: &str = "SPHERE_PRIMITIVE_MESH";
+const SPHERE_RESOLUTION: usize = 10;
 
 pub fn sphere_mesh_resource() -> MeshResourceID {
     MeshResourceID(SPHERE_PRIMITIVE_ID.to_owned())
@@ -250,19 +255,17 @@ fn configure_sphere_primitive(gpu: &AbstractGpu, a_server: &mut AssetServer) {
         Vector3::z(),        // Back
     ]
     .iter()
-    .map(|d| face(d, 10))
+    .map(|d| face(d, SPHERE_RESOLUTION))
     .unzip();
 
-    println!("{:?}", vertices);
-
     let vertices = vertices.into_iter().flatten().collect::<Vec<_>>();
-    //let indices = indices.into_iter().flatten().collect::<Vec<_>>();
     let indices = indices
         .iter()
         .enumerate()
         .flat_map(|(location, data)| {
-            data.iter()
-                .map(move |index| index + location as u16 * 10 * 10)
+            data.iter().map(move |index| {
+                index + location as u16 * SPHERE_RESOLUTION.pow(2) as u16
+            })
         })
         .collect::<Vec<_>>();
 
@@ -289,9 +292,8 @@ fn face(dir: &Vector3<f32>, resolution: usize) -> (Vec<Vertex>, Vec<u16>) {
             col: [0.0, 0.0, 0.0]
         };
         resolution.pow(2)
-    ]; //Vec::with_capacity(resolution.pow(2));
-       // Number of small faces, times 2 trianges per face, times 3 vertices.
-    let mut indices = vec![0; (resolution - 1).pow(2) * 2 * 3]; //Vec::with_capacity((resolution - 1).pow(2) * 2 * 3);
+    ];
+    let mut indices = vec![0; (resolution - 1).pow(2) * 2 * 3];
 
     let a_axis = dir.yzx();
     let b_axis = dir.cross(&a_axis);
@@ -331,6 +333,134 @@ fn face(dir: &Vector3<f32>, resolution: usize) -> (Vec<Vertex>, Vec<u16>) {
 
             i += 1;
         }
+    }
+
+    (vertices, indices)
+}
+
+// PLANE
+pub const PLANE_PRIMITIVE_ID: &str = "PLANE_PRIMITIVE_MESH";
+
+pub fn plane_mesh_resource() -> MeshResourceID {
+    MeshResourceID(PLANE_PRIMITIVE_ID.to_owned())
+}
+
+pub fn plane_mesh_component() -> MeshComponent {
+    MeshComponent(plane_mesh_resource())
+}
+
+const PLANE_VERTICES: &[Vertex] = &[
+    Vertex {
+        pos: [-1.0, 0.0, 1.0],
+        col: [0.0, 0.0, 1.0],
+    },
+    Vertex {
+        pos: [1.0, 0.0, 1.0],
+        col: [1.0, 0.0, 1.0],
+    },
+    Vertex {
+        pos: [1.0, 0.0, -1.0],
+        col: [1.0, 1.0, 1.0],
+    },
+    Vertex {
+        pos: [-1.0, 0.0, -1.0],
+        col: [0.0, 1.0, 1.0],
+    },
+];
+
+const PLANE_INDICES: &[u16] = &[0, 1, 2, 2, 3, 0];
+
+fn configure_plane_primitive(gpu: &AbstractGpu, a_server: &mut AssetServer) {
+    let v_buffer = gpu.allocate_vertex_buffer(
+        "Cube primitive vertices",
+        bytemuck::cast_slice(PLANE_VERTICES),
+    );
+
+    let i_buffer = gpu.allocate_index_buffer(
+        "Cube primitive indices",
+        bytemuck::cast_slice(PLANE_INDICES),
+    );
+
+    let mesh = Mesh::new(v_buffer, i_buffer, PLANE_INDICES.len() as u32);
+
+    a_server.register_mesh(PLANE_PRIMITIVE_ID.to_owned(), mesh);
+}
+
+// CONE
+pub const CONE_PRIMITIVE_ID: &str = "CYLINDER_PRIMITIVE_MESH";
+
+pub fn cone_mesh_resource() -> MeshResourceID {
+    MeshResourceID(CONE_PRIMITIVE_ID.to_owned())
+}
+
+pub fn cone_mesh_component() -> MeshComponent {
+    MeshComponent(cone_mesh_resource())
+}
+
+fn configure_cone_primitive(gpu: &AbstractGpu, a_server: &mut AssetServer) {
+    let (vertices, indices) = generate_cone_mesh(23);
+
+    let v_buffer = gpu.allocate_vertex_buffer(
+        "Cube primitive vertices",
+        bytemuck::cast_slice(&vertices),
+    );
+
+    let i_buffer = gpu.allocate_index_buffer(
+        "Cube primitive indices",
+        bytemuck::cast_slice(&indices),
+    );
+
+    let mesh = Mesh::new(v_buffer, i_buffer, indices.len() as u32);
+    a_server.register_mesh(CONE_PRIMITIVE_ID.to_owned(), mesh)
+}
+
+fn generate_cone_mesh(resolution: usize) -> (Vec<Vertex>, Vec<u16>) {
+    let mut vertices = vec![
+        Vertex {
+            pos: [0.0, 1.0, 0.0],
+            col: [0.0, 0.0, 0.0]
+        };
+        resolution + 1
+    ];
+
+    let mut index_cout = 0;
+    let mut indices: Vec<u16> = vec![0; resolution * 3 + (resolution - 2) * 3];
+
+    let angle = (360.0 as f32).to_radians() / resolution as f32;
+
+    for i in 1..resolution + 1 {
+        let local_angle = (i - 1) as f32 * angle;
+        let x = local_angle.cos();
+        let y = local_angle.sin();
+
+        vertices[i] = Vertex {
+            pos: [x, -1.0, y],
+            col: [x, 0.0, y],
+        };
+
+        indices[index_cout] = 0;
+        if i + 1 < resolution + 1 {
+            indices[index_cout + 1] = i as u16 + 1;
+        } else {
+            indices[index_cout + 1] = 1;
+        }
+        indices[index_cout + 2] = i as u16;
+
+        index_cout += 3;
+    }
+
+    for i in 2..vertices.len() - 1 {
+        indices[index_cout] = i as u16;
+
+        if i + 1 < resolution + 1 {
+            indices[index_cout + 1] = i as u16 + 1;
+        } else {
+            indices[index_cout + 1] = 1;
+        }
+
+        indices[index_cout + 2] = 1;
+
+        index_cout += 3;
     }
 
     (vertices, indices)
