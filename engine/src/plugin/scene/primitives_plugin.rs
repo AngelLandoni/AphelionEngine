e7992983a6ac741d75ec09afcb605bdd5f7d986b
Angel Landoni <angel.landoni.usa@gmail.com>
2024-03-11 15:07:40 +0100

    engine: Move resources to mod file

--- engine/src/plugin/scene/primitives_plugin.rs
+++ engine/src/plugin/scene/primitives_plugin.rs
@@ -6,7 +6,8 @@ use crate::{
     graphics::{
         components::MeshComponent, gpu::AbstractGpu, mesh::Mesh, vertex::Vertex,
     },
-    plugin::Pluggable, scene::assets::asset_server::{AssetServer, MeshResourceID},
+    plugin::Pluggable,
+    scene::assets::{asset_server::AssetServer, MeshResourceID},
 };
 
 /// Allocates and setups all the default primitives (Triangle, Quad, Cube, Cone,
